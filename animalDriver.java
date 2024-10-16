6b4bd3c1a474e0a2e466a4d59bd7e6c22e6d275d
FisherBR6 <fisherbachmanrhodes@gmail.com>
2024-10-16 14:41:03 -0700

    Update animalDriver.java

--- animalDriver.java
+++ animalDriver.java
@@ -5,7 +5,9 @@ public class animalDriver {
      * Main method
      */
     public static void main(String[] args) {
+        //create a Rabbit object
         Rabbit fella = new Rabbit("Jim");
+        //call the following 3 Rabbit methods
         fella.eat();
         fella.play();
         fella.sound();
