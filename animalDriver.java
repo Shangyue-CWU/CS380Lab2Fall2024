4ef3015cb3187f540579e6a53e8dd276a8e3e429
FisherBR6 <fisherbachmanrhodes@gmail.com>
2024-10-16 14:39:38 -0700

    Add files via upload

---
+++ animalDriver.java
@@ -0,0 +1,13 @@
+package CS380Lab2Fall2024;
+
+public class animalDriver {
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+        Rabbit fella = new Rabbit("Jim");
+        fella.eat();
+        fella.play();
+        fella.sound();
+    }
+}
