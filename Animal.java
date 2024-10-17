1b89598b0b1d34e9e2b21a845ecec9888af2d381
Ben Howell Burke <HowellBurkB@cwu.edu>
2024-10-16 18:03:46 -0700

    Added Iguana class implementation

--- Animal.java
+++ Animal.java
@@ -39,7 +39,52 @@ public abstract class Animal{
     * A  method that prints out that the animal is eating food.
     */
    public void eat(){
-      System.out.println(this.name + " eats some food.");
+      System.out.println(this.name + " eat some plants.");
+   }
+}
+
+/**
+ * Iguana class that extends the Animal abstract class.
+ * This class represents an iguana and its specific behaviors.
+ * @author Ben Howell-Burke
+ */
+class Iguana extends Animal {
+
+   /**
+    * Constructor for Iguana class.
+    * Calls the constructor of the Animal class.
+    * @param name Name of the iguana.
+    */
+   public Iguana(String name) {
+      super(name);  // Call the parent constructor to set the name
+   }
+
+   /**
+    * Override the sound method to represent the iguana's sound.
+    */
+   @Override
+   public void sound() {
+      System.out.println(this.name + " makes a soft hissing sound.");
+   }
+
+   /**
+    * Override the play method to represent how iguanas play.
+    */
+   @Override
+   public void play() {
+      System.out.println(this.name + " lay in the sun for fun.");
+   }
+
+   /**
+    * Main method to test the Iguana class implementation.
+    * @param args Command line arguments (not used).
+    */
+   public static void main(String[] args) {
+      // Create an Iguana object and test its methods
+      Iguana iggy = new Iguana("Iguanas");
+      iggy.sound();  // Iggy makes a soft hissing sound.
+      iggy.play();   // Iggy basks in the sun for fun.
+      iggy.eat();    // Iggy eats some food.
    }
 }
 
